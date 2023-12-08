e0d180ec30599ae067e6581969d4b1b35b0bbc17
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-12-08 12:57:49 -0500

    updated
    save for me

--- src/images/save_my/discounr-products.js
+++ src/images/save_my/discounr-products.js
@@ -1,73 +1,6 @@
-// import axios from 'axios';
-// import Notiflix from 'notiflix';
-// const BASE_URL = 'https://food-boutique.b.goit.study/api/products/discount';
-
-// async function searchImg() {
-//   return axios.get(`${BASE_URL}`).then(({ data }) => data);
-// }
-
-// function createMarkup(array) {
-//   return array
-//     .slice(0, 2)
-//     .map(
-//       ({
-//         _id,
-//         name,
-//         img,
-//         category,
-//         price,
-//         size,
-//         is10PercentOff,
-//         popularity,
-//       }) => {
-//         return `<div class="discount-card">
-//         <div class="discount-img-wrap">
-//             <a class="discount-gallery-link" href="${_id}">
-//                 <img src="${img}" alt="${name}" width="114" height="114" loading="lazy" />
-//             </a>
-//          <p class="discount-info-item">${name}      ${price}</p>
-//         </div>
-//         </div>`;
-//       }
-//     )
-//     .join('');
-// }
-
-// const refs = {
-//   discount: document.querySelector('.discount'),
-// };
-// const { discount } = refs;
-
-// let page = 1;
-// const paramDiscount = {
-//   position: 'top-right',
-//   timeout: 3000,
-//   width: '400px',
-//   fontSize: '30px',
-// };
-
-// function onSubmit() {
-//   page = 1;
-//   discount.innerHTML = '';
-//   searchImg()
-//     .then(data => {
-//       discount.innerHTML = createMarkup(data);
-//     })
-//     .catch(onSearchError);
-// }
-
-// function onSearchError() {
-//   Notiflix.Notify.failure(
-//     'Something went wrong! Please try again.',
-//     paramDiscount
-//   );
-// }
-
-// onSubmit();
-
 import { getDiscountProducts } from '../../services/food-api';
 
-const discount = document.querySelector('.discount');
+const discount = document.querySelector('.discount-list');
 const paramDiscount = {
   position: 'top-right',
   timeout: 3000,
@@ -105,14 +38,21 @@ function createMarkup(array) {
         is10PercentOff,
         popularity,
       }) => {
-        return `<div class="discount-card" data-product-id="${_id}">
+        return `<li class="discount-card" data-product-id="${_id}">
             <div class="discount-img-wrap">
+			<svg class="discount-icon" width="60" height="60">
+				<use href="./icons.svg#icon-discount"></use>
+			</svg>
                 <a class="discount-gallery-link" href="#">
                     <img src="${img}" alt="${name}" width="114" height="114" loading="lazy" />
                 </a>
-                <p class="discount-info-item">${name} ${price}</p>
+                <p class="discount-info-item">${name}</p>
+				<p class="discount-price-item">${price}</p>
+				<button class="add-cart-button">
+					<img src="cart_icon.png" alt="Add to Cart" />
+				</button>
             </div>
-        </div>`;
+        </li>`;
       }
     )
     .join('');
